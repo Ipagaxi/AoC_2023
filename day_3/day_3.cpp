25a998a50094e8791d98fabe2b8c93158e943e9f
Georg Thiesen <georg.thiesen@rwth-aachen.de>
2023-12-04 22:19:42 +0100

    day 3 part one completed

--- day_3/day_3.cpp
+++ day_3/day_3.cpp
@@ -6,48 +6,70 @@
 using namespace std;
 
 int getSumOfLegalValues(list<string> input_matrix) {
-    int sum;
+    int sum = 0;
     list<string>::iterator matrix_iter = input_matrix.begin();
-    string num;
-    int num_indices[2];
     int last_index = (*matrix_iter).length()-1;
     bool is_valid_num = false;
-    for (int x = 0; x < (*matrix_iter).length(); x++) {
-        if ((*matrix_iter)[x] >= 48 && (*matrix_iter)[x] <= 57) {
-            num_indices[0] = x;
-            int y = x;
-            // Get the complete number
-            while (((*matrix_iter)[y] >= 48) && ((*matrix_iter)[y] <= 57)) {
-                num += (*matrix_iter)[y];
-                num_indices[1] = y;
-                y++;
-            }
-            // Check if symbol exists before number
-            if (num_indices[0] != 0) {
-                // unicode(46) = '.'
-                if ((*matrix_iter)[num_indices[0]-1] != 46) {
-                    is_valid_num = true;
-                }
-            }
-            // Check if symbol exists after number
-            if (num_indices[1] != last_index) {
-                if ((*matrix_iter)[num_indices[1]+1] != 46) {
-                    is_valid_num = true;
-                }
-            }
-            // Check if symbol above number exists
-            if (matrix_iter != input_matrix.begin()) {
-                string top_line = matrix_iter.prev;
+    
+    for (matrix_iter = input_matrix.begin(); matrix_iter != input_matrix.end(); matrix_iter++) {
+        for (int x = 0; x < (*matrix_iter).length(); x++) {
+            is_valid_num = false;
 
-                for (int x = max(0, num_indices[0]-1); x <= min(last_index, num_indices[1]+1); x++) {
-                    if ()
+            if ((*matrix_iter)[x] >= 48 && (*matrix_iter)[x] <= 57) {
+                string num = "";
+                num += (*matrix_iter)[x];
+                int num_indices[2];
+                num_indices[0] = x;
+                num_indices[1] = x;
+                int y = x+1;
+                // Get the complete number
+                while (((*matrix_iter)[y] >= 48) && ((*matrix_iter)[y] <= 57)) {
+                    num += (*matrix_iter)[y];
+                    num_indices[1] = y;
+                    y++;
                 }
+
+                // Check if symbol exists before number
+                if (num_indices[0] != 0) {
+                    // unicode(46) = '.'
+                    if ((*matrix_iter)[num_indices[0]-1] != 46) {
+                        is_valid_num = true;
+                    }
+                }
+                // Check if symbol exists after number
+                if (num_indices[1] != last_index) {
+                    if ((*matrix_iter)[num_indices[1]+1] != 46) {
+                        is_valid_num = true;
+                    }
+                }
+                // Check if symbol above number exists
+                if (matrix_iter != input_matrix.begin()) {
+                    list<string>::iterator prev_iter = matrix_iter;
+                    prev_iter--;
+
+                    for (int x = max(0, num_indices[0]-1); x <= min(last_index, num_indices[1]+1); x++) {
+                        if (((*prev_iter)[x] < 48 || (*prev_iter)[x] > 57) && (*prev_iter)[x] != 46) {
+                            is_valid_num = true;
+                        }
+                    }
+                }
+                // Check if symbol under number exists
+                if (next(matrix_iter, 1) != input_matrix.end()) {
+                    list<string>::iterator next_iter = matrix_iter;
+                    next_iter++;
+
+                    for (int x = max(0, num_indices[0]-1); x <= min(last_index, num_indices[1]+1); x++) {
+                        if (((*next_iter)[x] < 48 || (*next_iter)[x] > 57) && (*next_iter)[x] != 46) {
+                            is_valid_num = true;
+                        }
+                    }
+                }
+                if (is_valid_num) {
+                    sum += stoi(num);
+                }
+                x = y-1;
             }
         }
-    }
-    while (matrix_iter != input_matrix.end()) {
-
-        matrix_iter++;
     }
 
     return sum;
@@ -62,7 +84,7 @@ int main() {
             input_matrix.push_back(line);
         }
         myfile.close();
-    } else cout << "Unable to open file";
+    } else cout << "Unable to open file" << endl;
 
     int sum = getSumOfLegalValues(input_matrix);
 
